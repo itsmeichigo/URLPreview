ead3ab15641cc593dae6b0885b5ec067cf14cb1f
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-08-26 22:09:12 +0800

    Updated using Alamofire - Removed deprecated code

--- Source/URL+Preview.swift
+++ Source/URL+Preview.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Kanna
+import Alamofire
 
 public extension URL {
     
@@ -17,55 +18,47 @@ public extension URL {
     
     func fetchPageInfo(_ completion: @escaping ((_ title: String?, _ description: String?, _ previewImage: String?) -> Void), failure: @escaping ((_ errorMessage: String) -> Void)) {
         
-        let request = NSMutableURLRequest(url: self)
         let newUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
-        request.setValue(newUserAgent, forHTTPHeaderField: "User-Agent")
+        
         ValidationQueue.queue.cancelAllOperations()
         
-        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: ValidationQueue.queue, completionHandler: { (response: URLResponse?, data: Data?, error: Error?) -> Void in
-            if error != nil {
+        request(self, method: .get, parameters: ["User-Agent": newUserAgent]).validate().responseData { response in
+            
+            if response.error != nil {
                 DispatchQueue.main.async(execute: {
                     failure("Url receive no response")
                 })
                 return
             }
             
-            if let urlResponse = response as? HTTPURLResponse {
-                if urlResponse.statusCode >= 200 && urlResponse.statusCode < 400 {
-                    if let data = data {
-                        
-                        if let doc = try? Kanna.HTML(html: data, encoding: String.Encoding.utf8) {
-                            let title = doc.title
-                            var description: String? = nil
-                            var previewImage: String? = nil
-                            
-                            if let nodes = doc.head?.xpath("//meta").enumerated() {
-                                for node in nodes {
-                                    if node.element["property"]?.contains("description") == true ||
-                                    node.element["name"] == "description" {
-                                        description = node.element["content"]
-                                    }
-                                    
-                                    if node.element["property"]?.contains("image") == true &&
-                                        node.element["content"]?.contains("http") == true {
-                                            previewImage = node.element["content"]
-                                    }
-                                }
+            if let data = response.data {
+                if let doc = try? Kanna.HTML(html: data, encoding: String.Encoding.utf8) {
+                    
+                    let title = doc.title
+                    
+                    var description: String? = nil
+                    
+                    var previewImage: String? = nil
+                    
+                    if let nodes = doc.head?.xpath("//meta").enumerated() {
+                        for node in nodes {
+                            if node.element["property"]?.contains("description") == true ||
+                                node.element["name"] == "description" {
+                                description = node.element["content"]
                             }
                             
-                            DispatchQueue.main.async(execute: {
-                                completion(title, description, previewImage)
-                            })
+                            if node.element["property"]?.contains("image") == true &&
+                                node.element["content"]?.contains("http") == true {
+                                previewImage = node.element["content"]
+                            }
                         }
-                        
                     }
-                } else {
+                    
                     DispatchQueue.main.async(execute: {
-                        failure("Url received \(urlResponse.statusCode) response")
+                        completion(title, description, previewImage)
                     })
-                    return
                 }
             }
-        })
+        }
     }
 }
